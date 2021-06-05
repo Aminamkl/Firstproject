12c01d8dd2a449e8f0a917716c59719d709ac538
Aminamkl <52087288+Aminamkl@users.noreply.github.com>
2021-06-05 14:53:17 +0100

    Add files via upload

---
+++ TD_JS/image.js
@@ -0,0 +1,12 @@
+function f(){
+    const boxes =document.querySelector('.myContainer .box');
+    for (var i=0; i<boxes.length; i++){
+        boxes[i].style.backgroundColor= getRandomColor();
+    }
+}
+
+function getRandomColor(){
+    const opacity = Math.random()
+    const 
+
+}
